9aa884ff6ee984132f065925101288daf6dede9e
TAB <tab@tab.com>
2023-03-20 22:49:04 -0400

    Update status checker to use channels

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -12,22 +13,34 @@ func main() {
 		"http://reddit.com",
 		"http://stackoverflow.com",
 		"http://golang.org",
-		"https://dev.zed",
-		"https://daredfjaghed.comd",
+		// "https://dev.zed",
+		// "https://daredfjaghed.comd",
 	}
 
+	c := make(chan string)
+
 	for _, url := range urlList {
-		checkUrl(url)
+		go checkUrl(url, c)
+	}
+
+	for l := range c {
+		go func(l string) {
+			time.Sleep(5 * time.Second)
+			checkUrl(l, c)
+		}(l)
 	}
 
 }
 
-func checkUrl(url string) {
+func checkUrl(url string, c chan string) {
 	_, err := http.Get(url)
 	if err != nil {
-		fmt.Println(url, "......ERR")
+		fmt.Println(url, "\t\t..... ERR")
+		c <- url
 		return
 	}
 
-	fmt.Println(url, "......OK")
+	fmt.Println(url, "\t\t..... OK")
+	c <- url
+
 }
